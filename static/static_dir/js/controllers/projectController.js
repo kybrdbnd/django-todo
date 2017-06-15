7cbdd8dc0bd01fea083b6388ffb1ea90081ee0d2
Pranav Puri <pranavpuri.p@hotmail.com>
2017-06-15 13:24:17 +0530

    serializers modified

--- static/static_dir/js/controllers/projectController.js
+++ static/static_dir/js/controllers/projectController.js
@@ -2,9 +2,15 @@ angular_module.controller('projectController', ['$scope', '$http', '$cookies', f
 
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+    $scope.summary_project = {};
+    $scope.summary_project['name'] = 'All Projects'
     $http.get('/api/').then(function(response) {
-        console.log(response.data)
         $scope.projects = response.data
-        // console.log($scope.projects)
+            // console.log($scope.projects)
+        $scope.summary_project['projects'] = response.data
     })
+    $scope.current_project = function() {
+        $scope.selected_project = event.currentTarget.innerHTML
+        console.log(event.currentTarget.innerHTML)
+    }
 }]);
--- todo/api/serializers.py
+++ todo/api/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from todo.models import (Company, Project, Task)
+from django.contrib.auth.models import User
+
+
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'username'
+
+        ]
 
 # company
 
@@ -37,7 +51,25 @@ class ProjectDetailSerializer(ModelSerializer):
         ]
 
 
+class TaskListSerializer(ModelSerializer):
+    created_by = UserListSerializer()
+    assigned_to = UserListSerializer()
+
+    class Meta:
+        model = Task
+        fields = [
+            'id',
+            'created_at',
+            'name',
+            'created_by',
+            'assigned_to'
+        ]
+
+
 class ProjectListSerializer(ModelSerializer):
+    task = TaskListSerializer(many=True)
+    members = UserListSerializer(many=True)
+
     class Meta:
         model = Project
         fields = [
@@ -47,10 +79,9 @@ class ProjectListSerializer(ModelSerializer):
             'members'
         ]
 
-# task
-
 
 class TaskCreateSerializer(ModelSerializer):
+
     class Meta:
         model = Task
         fields = [
@@ -70,15 +101,4 @@ class TaskDetailSerializer(ModelSerializer):
             'created_by',
             'assigned_to'
 
-        ]
-
-
-class TaskListSerializer(ModelSerializer):
-    class Meta:
-        model = Task
-        fields = [
-            'id',
-            'name',
-            'created_by',
-            'assigned_to'
         ]
