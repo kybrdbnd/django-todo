93f7501d994776f116ba6244813a06367086a48c
kybrdbnd <pranavpuri.p@hotmail.com>
2017-06-25 19:23:32 +0530

    database modified to postresql

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -99,11 +99,13 @@ WSGI_APPLICATION = 'todo_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_todo',
+        'USER': 'django_todo_user',
+        'PASSWORD': 'demo1234',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
